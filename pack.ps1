efb0c3fa9e8e58e54372a33f6f1ac105acd1847c
ikapper <ikapdev@gmail.com>
2024-10-11 12:08:47 +0900

    use 7zip instead of compress-archive to prevent being used backslash.

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,6 @@
-$compress = @{
-  Path = "_locales", "icons", "background.js", "manifest.json"
-  CompressionLevel = "Fastest"
-  DestinationPath = "output.zip"
-}
-Compress-Archive @compress
+$targets = @("_locales", "icons", "background.js", "manifest.json")
+
+# Set-Alias -Name 7zip -Value "C:\Program Files\7-Zip\7z.exe"
+# 上記を設定済みという想定。
+# Compress-Archiveではバックスラッシュが使われるのでダメ。
+7zip a -tzip output.zip @targets
